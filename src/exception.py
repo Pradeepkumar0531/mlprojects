2743eb323d839bfc222f2a325ac85347c3974a58
Pradeepkumar0531 <pradeepkumar000531@gmail.com>
2025-05-23 15:29:22 +0530

    notebook

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info() # gives you the info about filename, line number where the exception occurs
